e517a326a98e64062028270ec08e2a159f74d6e4
Matt Hunt <wmatthunt@gmail.com>
2020-12-31 12:11:33 -0500

    added personality

---
+++ client/src/arrays/personalityArray.js
@@ -0,0 +1,71 @@
+export const personalityArray = [
+    {
+        name: "Adventurer",
+        description: "Flexible and charming artists, always ready to explore and experience something new."
+    },
+    {
+        name: "Advocate",
+        description: "Quiet and mystical, yet very inspiring and tireless idealists."
+    },
+    {
+        name: "Architect",
+        description: "Imaginative and stragegic thinkers with a plan for everything."
+    },
+    {
+        name: "Campaigner",
+        description: "Enthusiastic, creative and sociable free spirts, who can always find a reason to smile."
+    },
+    {
+        name: "Commander",
+        description: "Bold, imaginative and strong-willed leaders, always finding a way - or making one."
+    },
+    {
+        name: "Consul",
+        description: "Extraordinarily caring, social and popular people, always eager to tell."
+    },
+    {
+        name: "Debator",
+        description: "Smart and curious thinkers who cannot resist an intellectual challenge."
+    },
+    {
+        name: "Defender",
+        description: "Very dedicated and warm protectors, always ready to defend their loved ones."
+    },
+    {
+        name: "Entertainer",
+        description: "Spontaneous, energetic and enthusiastic people - life if never boring around them."
+    },
+    {
+        name: "Entrepreneur",
+        description: "Smart, energetic and very perceptive people, who truly enjoy living on the edge."
+    },
+    {
+        name: "Executive",
+        description: "Excellant administrators, unsurpassed at managing things - or people."
+    },
+    {
+        name: "Logician",
+        description: "Innovative inventors with an unquenchable thirst for knowledge."
+    },
+    {
+        name: "Logistician",
+        description: "Practical and fact-minded individuals, whose reliability cannot be doubted."
+    },
+    {
+        name: "Mediator",
+        description: "Poetic and altruistic people, always eager to help a good cause."
+    },
+    {
+        name: "Protagonist",
+        description: "Charismatic and inspiring leaders, able to mesmerize their listeners."
+    },
+    {
+        name: "Virtuoso",
+        description: "Bold and practical experimenters, master of all kinds of tools."
+    },
+    {
+        name: "",
+        description: ""
+    },
+]
+export default personalityArray;
--- client/src/components/Generator/index.js
+++ client/src/components/Generator/index.js
@@ -4,6 +4,7 @@ import ageArray from "../../arrays/ageArray.js";
 import genderArray from "../../arrays/genderArray.js";
 import raceArray from "../../arrays/raceArray.js";
 import jobArray from "../../arrays/jobArray.js";
+import personalityArray from "../../arrays/personalityArray.js";
 import motivationArray from "../../arrays/motivationArray.js";
 
 function Generator() {
@@ -12,7 +13,8 @@ function Generator() {
     const [race, setRace] = useState(raceArray[0]);
     const [subRace, setSubRace] = useState(raceArray[0].subRace[Math.floor((Math.random() * raceArray[0].subRace.length))]);
     const [job, setJob] = useState(jobArray[Math.floor((Math.random() * jobArray.length))]);
-    const [motivation, setIdeal] = useState(motivationArray[Math.floor((Math.random() * motivationArray.length))]);
+    const [personality, setPersonality] = useState(personalityArray[Math.floor((Math.random() * personalityArray.length))]);
+    const [motivation, setMotivation] = useState(motivationArray[Math.floor((Math.random() * motivationArray.length))]);
     const [npc, setNPC] = useState(age + " " + gender + " " + subRace + " " + race.raceName + " the " + job.jobName);
     const randomNPC = () => {
         const randomAge = Math.floor((Math.random() * ageArray.length));
@@ -24,9 +26,11 @@ function Generator() {
         const randomSubRace = raceArray[randomRace].subRace[Math.floor((Math.random() * raceArray[randomRace].subRace.length))]
         setSubRace(randomSubRace);
         const randomJob = Math.floor((Math.random() * jobArray.length));
-        setJob(jobArray[randomJob])
+        setJob(jobArray[randomJob]);
+        const randomPersonality = Math.floor((Math.random() * personalityArray.length));
+        setPersonality(personalityArray[randomPersonality]);
         const randomMotivation = Math.floor((Math.random() * motivationArray.length));
-        setIdeal(motivationArray[randomMotivation]);
+        setMotivation(motivationArray[randomMotivation]);
         setNPC(age + " " + gender + " " + subRace + " " + race.raceName + " the " + job.jobName);
     };
     return (
@@ -39,8 +43,10 @@ function Generator() {
             {/* first time is always undefined */}
             <h2>Character Sheet</h2>
             <p>{npc}</p>
-            <h3>Description:</h3>
+            <h3>Description</h3>
             <p></p>
+            <h3>Personality</h3>
+            <p>{personality.name} - {personality.description}</p>
             <h3>Motivation</h3>
             <p>{motivation.name} - {motivation.description}</p>
             <button onClick={() => randomNPC()}>Generate New NPC</button>
